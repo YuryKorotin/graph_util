d6758ce1c64dc5bbe66dba2a5e27501450f4e81d
SUPERUSER <superuser@SUPERUSERs-MacBook-Pro.local>
2019-09-08 10:47:43 +0400

    Fix wolfram repository

--- lib/src/repositories/wolframalpha_repository.dart
+++ lib/src/repositories/wolframalpha_repository.dart
@@ -3,8 +3,7 @@ import 'package:graph_util/src/models/expression_result.dart';
 class WolframalphaRepository {
    Future<ExpressionResult> getExpressionResult() async {
 
-     await Future.delayed(Duration(seconds: 2));
-
-     return ExpressionResult([1.2, 1.3]);
+     return Future.delayed(
+         Duration(seconds: 2), () => ExpressionResult([1.2, 1.3]));
    }
 }
