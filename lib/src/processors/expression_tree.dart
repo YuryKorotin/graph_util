0504ba177c4da4c46cf96d1c744fee82898febbf
YuryKorotin <dreadnoughtfull@yandex.ru>
2019-09-10 18:19:26 +0400

    Add evaluation of expression

--- lib/src/processors/expression_tree.dart
+++ lib/src/processors/expression_tree.dart
@@ -1,11 +1,67 @@
-class ExpressionTree {
-  String _expression;
+import 'package:graph_util/src/processors/expression_node.dart';
 
-  ExpressionTree(this._expression);
+class ExpressionTree {
+  List<String> _expression;
+  List<String> _expressionToEvaluate;
+  ExpressionNode _root;
+
+  ExpressionTree(String expression) {
+    this._expression = expression.split(" ");
+  }
 
   bool isOperator(String symbol) {
     var symbolOperatorString = "+-*/";
 
     return symbolOperatorString.contains(symbol);
+  }
+
+  bool isParameter(String symbol) {
+    var parameters = ["x"];
+
+    return parameters.contains(symbol);
+  }
+
+  bool isNumber(String symbol) {
+    if(symbol == null) {
+      return false;
+    }
+    return
+        // ignore: deprecated_member_use
+        int.parse(symbol, onError: (e) => null) != null;
+  }
+
+  ExpressionNode getRoot() {
+    return _root;
+  }
+
+  void startEvaluation() {
+    _expressionToEvaluate = List.of(_expression);
+    _root = evaluate();
+  }
+  
+  ExpressionNode evaluate() {
+    String symbol = _expressionToEvaluate[0];
+    _expressionToEvaluate.removeAt(0);
+    ExpressionNode node = new ExpressionNode(symbol);
+
+    if (isNumber(symbol) || isParameter(symbol)) {
+      node.left = null;
+      node.right = null;
+    } else if (symbol == '(') {
+      node.left = evaluate();
+      node.value = _expressionToEvaluate[0];
+      _expressionToEvaluate.removeAt(0);
+      node.right = evaluate();
+      symbol = _expressionToEvaluate[0];
+      _expressionToEvaluate.removeAt(0);
+      if (symbol != ')') {
+        print("PARSING ERROR");
+      }
+    } else {
+      print("PARSING ERROR");
+      return null;
+    }
+
+    return node;
   }
 }
--- test/processors/expression_tree_test.dart
+++ test/processors/expression_tree_test.dart
@@ -3,12 +3,32 @@ import 'package:graph_util/src/processors/expression_tree.dart';
 
 void main() {
   test('Test expression tree isOperator', () {
-    var tree = ExpressionTree("x + e");
+    var tree = ExpressionTree("x + 5");
 
     expect(true, tree.isOperator("+"));
     expect(true, tree.isOperator("-"));
     expect(true, tree.isOperator("*"));
     expect(true, tree.isOperator("/"));
     expect(false, tree.isOperator("s"));
+  });
+
+  test('Test isParameter', () {
+    var tree = ExpressionTree("x + 5");
+
+    expect(true, tree.isParameter("x"));
+  });
+
+  test('Test isNumber', () {
+    var tree = ExpressionTree("x + 5");
+
+    expect(true, tree.isNumber("23"));
+  });
+
+  test('Test expression tree parseExpression', () {
+    var tree = ExpressionTree("(x + 4)");
+
+    tree.startEvaluation();
+
+    expect("+", tree.getRoot().value);
   });
 }
